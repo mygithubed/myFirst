47419f939610337fd27e350ec78c60574152e8b2
zengzhipeng <2493360725@qq.com>
2019-06-09 17:50:01 +0800

    添加了一个类

---
+++ src/com/it/Test.java
@@ -0,0 +1,14 @@
+package com.it;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Author:曾志鹏
+ * Date:2019/6/9
+ * Time:17:48
+ */
+public class Test {
+    public void tested(){
+        System.out.println("实现的方法！！");
+    }
+}
