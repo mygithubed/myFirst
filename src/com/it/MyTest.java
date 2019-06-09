f0e8dc977a38fce51325fa08cfdec788c999494f
zengzhipeng <2493360725@qq.com>
2019-06-09 17:36:29 +0800

    Initial commit

---
+++ src/com/it/MyTest.java
@@ -0,0 +1,13 @@
+package com.it;
+
+/**
+ * Author:曾志鹏
+ * Date:2019/6/9
+ * Time:17:34
+ */
+public class MyTest {
+    public static void main(String[] args)
+    {
+        System.out.println("新的一个类！");
+    }
+}
